21006ab283295b608e7f5cc18892977246545041
MahmoudHany <mahmoud.hany.attia@gmail>
2025-03-13 07:05:48 +0200

    BlocConsumer

--- basket_ball/lib/main.dart
+++ basket_ball/lib/main.dart
@@ -1,245 +1,231 @@
 // import 'package:flutter/foundation.dart';
+import 'package:basket_ball/cubits/counter_cubit.dart';
+import 'package:basket_ball/cubits/counter_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const PointsCounter());
 }
 
-class PointsCounter extends StatefulWidget {
+class PointsCounter extends StatelessWidget {
   const PointsCounter({super.key});
 
   @override
-  State<PointsCounter> createState() => _PointsCounterState();
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
+      ),
+    );
+  }
 }
 
-class _PointsCounterState extends State<PointsCounter> {
+class HomePage extends StatelessWidget {
+   HomePage({super.key});
   int teamAScore = 0;
   int teamBScore = 0;
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: const Text('points Counter'),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                SizedBox(
-                  height: 500,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      const Text(
-                        'Team A',
-                        style: TextStyle(
-                          fontSize: 33,
-                        ),
-                      ),
-                      Text(
-                        '$teamAScore',
-                        style: const TextStyle(
-                          fontSize: 150,
-                        ),
-                      ),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.blue,
-                          minimumSize: const Size(120, 50),
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            teamAScore += 1;
-                          });
-                        },
-                        child: const Text(
-                          '1 points',
+    return BlocConsumer<CounterCubit, CounterState>(
+      listener: (context, state) {
+        if (state is CounterAIncrementState) {
+          teamAScore =BlocProvider.of<CounterCubit>(context).teamAScore;
+        }
+        else {
+          teamBScore = BlocProvider.of<CounterCubit>(context).teamBScore;
+        }
+      },
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blue,
+            title: const Text('points Counter'),
+          ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SizedBox(
+                    height: 500,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        const Text(
+                          'Team A',
                           style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 18,
+                            fontSize: 33,
                           ),
                         ),
-                      ),
-                      // SizedBox(
-                      //   height: 10,
-                      // ),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          padding: const EdgeInsets.all(8),
-                          backgroundColor: Colors.blue,
-                          minimumSize: const Size(120, 50),
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            teamAScore += 2;
-                          });
-                        },
-                        child: const Text(
-                          '2 points',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 18,
+                        Text(
+                          '$teamAScore',
+                          style: const TextStyle(
+                            fontSize: 150,
                           ),
                         ),
-                      ),
-                      // SizedBox(
-                      //   height: 10,
-                      // ),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.blue,
-                          minimumSize: Size(120, 50),
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            teamAScore += 3;
-                            print('Team A: $teamAScore');
-                          });
-                        },
-                        child: Text(
-                          '3 points',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 18,
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue,
+                            minimumSize: const Size(120, 50),
+                          ),
+                          onPressed: () {},
+                          child: const Text(
+                            '1 points',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18,
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                        // SizedBox(
+                        //   height: 10,
+                        // ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.all(8),
+                            backgroundColor: Colors.blue,
+                            minimumSize: const Size(120, 50),
+                          ),
+                          onPressed: () {},
+                          child: const Text(
+                            '2 points',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                        // SizedBox(
+                        //   height: 10,
+                        // ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue,
+                            minimumSize: Size(120, 50),
+                          ),
+                          onPressed: () {},
+                          child: Text(
+                            '3 points',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                Container(
-                  height: 500,
-                  child: VerticalDivider(
-                    color: Colors.blue,
-                    thickness: 1,
-                    endIndent: 50,
-                    indent: 50,
+                  Container(
+                    height: 500,
+                    child: VerticalDivider(
+                      color: Colors.blue,
+                      thickness: 1,
+                      endIndent: 50,
+                      indent: 50,
+                    ),
                   ),
-                ),
-                Container(
-                  height: 500,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Text(
-                        'Team B',
-                        style: TextStyle(
-                          fontSize: 33,
-                        ),
-                      ),
-                      Text(
-                        '$teamBScore',
-                        style: TextStyle(
-                          fontSize: 150,
-                        ),
-                      ),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          padding: EdgeInsets.all(8),
-                          backgroundColor: Colors.blue,
-                          minimumSize: Size(120, 50),
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            teamBScore += 1;
-                            print('Team B: $teamBScore');
-                          });
-                        },
-                        child: Text(
-                          '1 points',
+                  Container(
+                    height: 500,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          'Team B',
                           style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 18,
+                            fontSize: 33,
                           ),
                         ),
-                      ),
-                      // SizedBox(
-                      //   height: 10,
-                      // ),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.blue,
-                          minimumSize: Size(120, 50),
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            teamBScore += 2;
-                            print('Team B: $teamBScore');
-                          });
-                        },
-                        child: Text(
-                          '2 points',
+                        Text(
+                          '$teamBScore',
                           style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 18,
+                            fontSize: 150,
                           ),
                         ),
-                      ),
-                      // SizedBox(
-                      //   height: 10,
-                      // ),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.blue,
-                          minimumSize: Size(120, 50),
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            teamBScore += 3;
-                            print('Team B: $teamBScore');
-                          });
-                        },
-                        child: Text(
-                          '3 points',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 18,
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            padding: EdgeInsets.all(8),
+                            backgroundColor: Colors.blue,
+                            minimumSize: Size(120, 50),
+                          ),
+                          onPressed: () {},
+                          child: Text(
+                            '1 points',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18,
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                        // SizedBox(
+                        //   height: 10,
+                        // ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue,
+                            minimumSize: Size(120, 50),
+                          ),
+                          onPressed: () {},
+                          child: Text(
+                            '2 points',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                        // SizedBox(
+                        //   height: 10,
+                        // ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue,
+                            minimumSize: Size(120, 50),
+                          ),
+                          onPressed: () {},
+                          child: Text(
+                            '3 points',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 32,
-            ),
-            Spacer(
-              flex: 2,
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue,
-                minimumSize: Size(120, 50),
+                ],
               ),
-              onPressed: () {
-                setState(() {
-                  teamAScore = 0;
-                  teamBScore = 0;
-                  print('Reset');
-                });
-              },
-              child: Text(
-                'Reset',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18,
+              SizedBox(
+                height: 32,
+              ),
+              Spacer(
+                flex: 2,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  minimumSize: Size(120, 50),
+                ),
+                onPressed: () {},
+                child: Text(
+                  'Reset',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                  ),
                 ),
               ),
-            ),
-            Spacer(
-              flex: 4,
-            ),
-          ],
-        ),
-      ),
+              Spacer(
+                flex: 4,
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
